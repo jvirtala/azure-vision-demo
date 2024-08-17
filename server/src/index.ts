65122bcc624d0f0dbcbda06f78de7b4a8527b350
Jani Virtala <jani.virtala@iki.fi>
2024-08-17 19:18:08 +0300

    added sentimentAnalysis api

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@
 import express, { Express, Request, Response } from 'express';
 import multer from 'multer';
 import { analyzePhoto } from './PhotoAnalysis';
+import { analyzeSentiment } from './SentimentAnalysis';
 
 const app: Express = express();
 const port: number = 3000; // You can change this port number if needed
@@ -23,6 +24,24 @@ app.post('/photoanalysis', upload.single('file'), async (req: Request, res: Resp
     res.status(500).send('An error occurred while analyzing the photo.');
   }
 });
+
+app.get('/sentiment', async (req: Request, res: Response) => {
+  try {
+    const text = req.query.text;
+
+    if (!text || typeof text !== 'string') {
+      return res.status(400).send('Text query parameter is required and must be a string.');
+    }
+
+    const result = await analyzeSentiment(text);
+
+    res.json(result);
+  } catch (error) {
+    console.error('Error analyzing sentiment:', error);
+    res.status(500).send('An error occurred while analyzing the sentiment.');
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);

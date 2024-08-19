2ad048b368982131a36d680abcceeae4a7676075
Jani Virtala <jani.virtala@quentic.com>
2024-08-19 16:45:21 +0300

    refactored sentiment analysis result

--- client/src/routes/PhotoAnalysis/AnalysisResultDisplay.tsx
+++ client/src/routes/PhotoAnalysis/AnalysisResultDisplay.tsx
@@ -16,6 +16,16 @@ export interface AnalysisResultDisplayProps {
   result: AnalysisResult;
 }
 
+const bytesToHumanReadable = (bytes: number): string => {
+  if (bytes < 1024) {
+    return `${bytes} bytes`;
+  } else if (bytes < 1024 * 1024) {
+    return `${(bytes / 1024).toFixed(2)} KB`;
+  } else {
+    return `${(bytes / (1024 * 1024)).toFixed(2)} MB`;
+  }
+};
+
 const AnalysisResultDisplay: React.FC<AnalysisResultDisplayProps> = ({
   result,
 }) => {
@@ -23,9 +33,9 @@ const AnalysisResultDisplay: React.FC<AnalysisResultDisplayProps> = ({
     <div className="bg-white shadow-md rounded-lg p-6 max-w-md mx-auto">
       <h2 className="text-2xl font-bold mb-4">Analysis Result</h2>
       <p className="mb-4">
-        <strong>Size:</strong> {result.size}
+        <strong>Size:</strong> {bytesToHumanReadable(result.size)}
       </p>
-      <h3 className="text-xl font-semibold mb-2">Detected Objects:</h3>
+      <h3 className="text-xl font-semibold mb-2 from-1">Detected Objects:</h3>
       <ul className="space-y-2">
         {result.objects.map((obj, index) => (
           <ObjectResult obj={obj} index={index} />
--- client/src/routes/SentimentAnalysis/AnalysisResultDisplay.tsx
+++ client/src/routes/SentimentAnalysis/AnalysisResultDisplay.tsx
@@ -13,9 +13,27 @@ type AnalysisResultDisplayProps = {
   data: AnalysisResult;
 };
 
+const createStyle = (confidence: number) => {
+  return {
+    width: `${(confidence * 100).toFixed(0)}%`,
+  };
+};
+
 const AnalysisResultDisplay: React.FC<AnalysisResultDisplayProps> = ({
   data,
 }) => {
+  const neutralStyle = {
+    width: `${createStyle(data.confidenceScores.neutral)}%`,
+  };
+
+  const positiveStyle = {
+    width: `${createStyle(data.confidenceScores.positive)}%`,
+  };
+
+  const negativeStyle = {
+    width: `${createStyle(data.confidenceScores.negative)}%`,
+  };
+
   return (
     <div className="bg-white shadow-md rounded-lg p-6 max-w-md mx-auto">
       <h2 className="text-2xl font-bold mb-4">Analysis Result</h2>
@@ -24,15 +42,27 @@ const AnalysisResultDisplay: React.FC<AnalysisResultDisplayProps> = ({
       </p>
       <h3 className="text-xl font-semibold mb-2">Detected sentiment:</h3>
       <div className="space-y-2">
-        <p className={"bg-green-400"}>
-          Positive: {(data.confidenceScores.positive * 100).toFixed(2)}%
-        </p>
-        <p className={"bg-yellow-400"}>
-          Neutral: {(data.confidenceScores.neutral * 100).toFixed(2)}%
-        </p>
-        <p className={"bg-red-400"}>
-          Negative: {(data.confidenceScores.negative * 100).toFixed(2)}%
-        </p>
+        <p className="mb-4">Positive</p>
+        <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+          <div
+            className="bg-green-400 h-2.5 rounded-full"
+            style={positiveStyle}
+          ></div>
+        </div>
+        <p className="mb-4">Neutral</p>
+        <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+          <div
+            className="bg-yellow-400 h-2.5 rounded-full"
+            style={neutralStyle}
+          ></div>
+        </div>
+        <p className="mb-4">Negative</p>
+        <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+          <div
+            className="bg-red-400 h-2.5 rounded-full"
+            style={negativeStyle}
+          ></div>
+        </div>
       </div>
     </div>
   );
